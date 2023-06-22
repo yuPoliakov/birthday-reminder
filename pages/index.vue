9250b170268bf19c6ad4febb2346dfb5241708bb
Yurii <Swivt888@gmail.com>
2023-06-22 16:31:06 +0300

    Initial

--- pages/index.vue
+++ pages/index.vue
@@ -35,26 +35,30 @@
         </div>
         <div class="flex justify-center mt-20 font-mono">
             <div class="w-3/4 sm:w-2/3 lg:w-1/3 sm:space-y-4">
-                <form action="" method="post" @submit.prevent="submitForm">
+                <Form action="" method="post" @submit.prevent="submitForm">
                     <div class="flex flex-wrap text-paradise">
                         <div class="w-full">
                             <label for="email">Your email</label>
-                            <input type="email" v-model="formFields.email" name="email" id="email" required class="w-full h-14 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline">
+                            <Field :rules="validateEmail" type="email" v-model="formFields.email" name="email" id="email" required class="w-full h-14 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" />
+                            <ErrorMessage name="email" class="validation-error" />
                         </div>
                     </div>
 
                     <div class="flex flex-wrap mt-9 text-paradise">
                         <div class="w-3/4 sm:pr-2 sm:w-1/3">
                             <label for="day">Birthday</label>
-                            <input type="text" v-model="formFields.day" name="day" id="day" required class="w-full h-14 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline">
+                            <Field type="text" v-model="formFields.day" :rules="validateDay" name="day" id="day" required class="w-full h-14 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" />
+                            <ErrorMessage name="day" class="validation-error" />
                         </div>
                         <div class="w-3/4 sm:px-2 sm:w-1/3">
                             <label for="month">Birth month</label>
-                            <input type="text" v-model="formFields.month" name="month" id="month" required class="w-full h-14 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline">
+                            <Field type="text" v-model="formFields.month" :rules="validateMonth" name="month" id="month" required class="w-full h-14 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" />
+                            <ErrorMessage name="month" class="validation-error"/>
                         </div>
                         <div class="w-3/4 sm:pl-2 sm:w-1/3">
                             <label for="year">Birth year</label>
-                            <input type="text" v-model="formFields.year" name="year" id="year" class="w-full h-14 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline">
+                            <Field type="text" v-model="formFields.year" :rules="validateYear" name="year" id="year" class="w-full h-14 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" />
+                            <ErrorMessage name="year" class="validation-error" />
                         </div>
                     </div>
 
@@ -67,10 +71,11 @@
 
                     <div class="flex flex-wrap mt-9 text-paradise">
                         <div class="w-2/12 pr-2 sm:w-1/12">
-                            <input type="text" v-model="formFields.daysBefore" name="daysBefore" id="daysBefore" class="w-full h-12 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline">
+                            <Field type="text" :rules="validateDaysBefore" v-model="formFields.daysBefore" name="daysBefore" id="daysBefore" class="w-full h-12 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline" />
                         </div>
                         <div class="w-10/12 pr-2 sm:w-11/12">
                             <label for="daysBefore">Specify days before you want to get a reminder. If not specified we will send you a reminder on the same day.</label>
+                            <ErrorMessage name="daysBefore" class="validation-error"/>
                         </div>
                     </div>
 
@@ -85,8 +90,11 @@
 </template>
 
 <script setup>
+    import { Field, Form, ErrorMessage } from 'vee-validate';
+
     const errorMsg = ref('');
     const isSubmitted = ref(false);
+
     const formFieldsInitial = reactive({
         email: '',
         day: '',
@@ -121,6 +129,85 @@
     function resetForm() {
       Object.assign(formFields, formFieldsInitial);
     }
+
+    function validateEmail(value) {
+        if (!value) {
+            return 'Email is required';
+        }
+
+        const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+        if (!regex.test(value)) {
+            return 'Please enter valid email';
+        }
+
+        return true;
+    }
+
+    function validateDay(value) {
+        if (!value) {
+            return 'The day is required';
+        }
+
+        const number = parseInt(value.replaceAll(/\s/g, ''));
+
+        if (Number.isNaN(number)){
+            return 'Please type number';
+        }
+
+        if (number < 1 || number > 31) {
+            return 'Day must not exceeded 31';
+        }
+
+        return true;
+    }
+
+    function validateMonth(value) {
+        if (!value) {
+            return 'The month is required';
+        }
+
+        const number = parseInt(value.replaceAll(/\s/g, ''));
+
+        if (Number.isNaN(number)){
+            return 'Please type number';
+        }
+
+        if (number < 1 || number > 12) {
+            return 'Month must not exceeded 12';
+        }
+
+        return true;
+    }
+
+    function validateYear(value) {
+        if (!value) {
+            return true;
+        }
+
+        if (!/^(19[5-9]\d|20[0-4]\d|2050)$/.test(value)) {
+            return 'Enter valid year';
+        }
+
+        return true;
+    }
+
+    function validateDaysBefore(value) {
+        if (!value) {
+            return true;
+        }
+
+        const number = parseInt(value.replaceAll(/\s/g, ''));
+
+        if (Number.isNaN(number)){
+            return 'Please type number';
+        }
+
+        if (number < 1 || number > 7) {
+            return 'Days before must not exceeded 7';
+        }
+
+        return true;
+    }
 </script>
 
 <style>
@@ -129,5 +216,8 @@
     }
     .border-paradise {
         border-color: rgb(0 220 130 / 1);
+    }
+    .validation-error {
+        color: red
     }
 </style>
